6af072656f5be9dd9dda42979f673eaf7272ab87
clever.ch <clever.chuky@gmail.com>
2019-09-21 16:17:00 -0300

    Update Customer.java

--- desapp-groupc1f-backend/src/main/java/model/Customer.java
+++ desapp-groupc1f-backend/src/main/java/model/Customer.java
@@ -3,6 +3,30 @@ package model;
 import java.util.List;
 
 public class Customer extends User {
-    List<Order> order;
-    List<OrderHistory> orderHistory;
+    private List<Order> order;
+    private List<OrderHistory> orderHistory;
+    
+	public List<Order> getOrder() {
+		return order;
+	}
+	
+	public void setOrder(List<Order> orders) {
+		this.order = orders;
+	}
+	
+	public void addOrder(Order order) {
+		this.order.add(order);
+	}
+	
+	public List<OrderHistory> getOrderHistory() {
+		return orderHistory;
+	}
+	
+	public void setOrderHistory(List<OrderHistory> orderHistories) {
+		this.orderHistory = orderHistories;
+	}
+	
+	public void addOrderHistory(OrderHistory orderHistory) {
+		this.orderHistory.add(orderHistory);
+	}
 }
