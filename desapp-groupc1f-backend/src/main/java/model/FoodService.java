35e3a9d69d6fd1f4420fd5bb4b679b2024626baa
clever.ch <clever.chuky@gmail.com>
2019-09-21 16:17:05 -0300

    Update FoodService.java

--- desapp-groupc1f-backend/src/main/java/model/FoodService.java
+++ desapp-groupc1f-backend/src/main/java/model/FoodService.java
@@ -1,5 +1,117 @@
 package model;
 
-public class FoodService {
+import java.util.List;
 
+import utilities.Entity;
+
+public class FoodService extends Entity {
+	private String serviceName;
+	private String serviceLocation;
+	private String serviceAddress;
+	private String locationPointMap;	//TODO: Por ahora se deja como string. Checkear formato API para las direcciones
+	private String serviceDescription;
+	private String webSite;
+	private String email;
+	private String phone;
+	private List<ServiceHours> serviceHours;
+	private String deliveryLocations;
+	private List<Menu> serviceMenues;
+	private List<FoodServiceScore> serviceScores;
+	
+	
+	public String getServiceName() {
+		return serviceName;
+	}
+	
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public String getServiceLocation() {
+		return serviceLocation;
+	}
+
+	public void setServiceLocation(String serviceLocation) {
+		this.serviceLocation = serviceLocation;
+	}
+
+	public String getServiceAddress() {
+		return serviceAddress;
+	}
+
+	public void setServiceAddress(String serviceAddress) {
+		this.serviceAddress = serviceAddress;
+	}
+
+	public String getLocationPointMap() {
+		return locationPointMap;
+	}
+
+	public void setLocationPointMap(String locationPointMap) {
+		this.locationPointMap = locationPointMap;
+	}
+
+	public String getServiceDescription() {
+		return serviceDescription;
+	}
+
+	public void setServiceDescription(String serviceDescription) {
+		this.serviceDescription = serviceDescription;
+	}
+
+	public String getWebSite() {
+		return webSite;
+	}
+
+	public void setWebSite(String webSite) {
+		this.webSite = webSite;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public List<ServiceHours> getServiceHours() {
+		return serviceHours;
+	}
+
+	public void addServiceHours(ServiceHours serviceHour) {
+		this.serviceHours.add(serviceHour);
+	}
+
+	public String getDeliveryLocations() {
+		return deliveryLocations;
+	}
+
+	public void setDeliveryLocations(String deliveryLocations) {
+		this.deliveryLocations = deliveryLocations;
+	}
+
+	public List<Menu> getServiceMenues() {
+		return serviceMenues;
+	}
+
+	public void addServiceMenue(Menu menu) {
+		this.serviceMenues.add(menu);
+	}
+
+	public List<FoodServiceScore> getServiceScores() {
+		return serviceScores;
+	}
+
+	public void addServiceScores(FoodServiceScore serviceScore) {
+		this.serviceScores.add(serviceScore);
+	}
 }
