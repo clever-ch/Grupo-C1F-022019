666fed871e1b0cfb2182ea5495cc37c7d69f1498
clever.ch <clever.chuky@gmail.com>
2019-09-21 16:17:20 -0300

    Update OrderHistory.java

--- desapp-groupc1f-backend/src/main/java/model/OrderHistory.java
+++ desapp-groupc1f-backend/src/main/java/model/OrderHistory.java
@@ -1,5 +1,25 @@
 package model;
 
-public class OrderHistory {
+import utilities.Entity;
 
+public class OrderHistory extends Entity {
+	private User user;
+	private Order order;
+	
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
 }
