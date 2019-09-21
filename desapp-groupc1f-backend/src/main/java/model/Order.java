897953edb90f2b34bf63815fd31b79dad4858bcd
clever.ch <clever.chuky@gmail.com>
2019-09-21 16:17:17 -0300

    Update Order.java

--- desapp-groupc1f-backend/src/main/java/model/Order.java
+++ desapp-groupc1f-backend/src/main/java/model/Order.java
@@ -3,14 +3,64 @@ package model;
 import java.util.ArrayList;
 import java.util.Date;
 import constants.DeliveryState;
+import utilities.Entity;
 import java.time.LocalTime;
 
 
-public class Order {
-	ArrayList<Tuple> MenuAmount = new ArrayList<Tuple>();	
-    boolean hasDelivery;
-    Date creationDate;
-    Date deliveredDate;
-    LocalTime deliveryTime;
-    DeliveryState deliveryState;
+public class Order extends Entity {
+	private ArrayList<OrderItem> orderItems = new ArrayList<OrderItem>();	
+	private boolean hasDelivery;
+	private Date creationDate;
+	private Date deliveredDate;
+	private LocalTime deliveryTime;
+	private DeliveryState deliveryState;
+	
+	
+	public ArrayList<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+	
+	public void addOrderItems(OrderItem orderItem) {
+		this.orderItems.add(orderItem);
+	}
+
+	public boolean isHasDelivery() {
+		return hasDelivery;
+	}
+
+	public void setHasDelivery(boolean hasDelivery) {
+		this.hasDelivery = hasDelivery;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Date getDeliveredDate() {
+		return deliveredDate;
+	}
+
+	public void setDeliveredDate(Date deliveredDate) {
+		this.deliveredDate = deliveredDate;
+	}
+
+	public LocalTime getDeliveryTime() {
+		return deliveryTime;
+	}
+
+	public void setDeliveryTime(LocalTime deliveryTime) {
+		this.deliveryTime = deliveryTime;
+	}
+
+	public DeliveryState getDeliveryState() {
+		return deliveryState;
+	}
+
+	public void setDeliveryState(DeliveryState deliveryState) {
+		this.deliveryState = deliveryState;
+	}
 }
