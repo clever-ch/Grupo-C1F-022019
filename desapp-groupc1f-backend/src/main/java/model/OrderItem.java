c83c0f987d3917b1e1e3f5fb78cffc2504931065
clever.ch <clever.chuky@gmail.com>
2019-09-22 11:39:43 -0300

    Update OrderItem.java

--- desapp-groupc1f-backend/src/main/java/model/OrderItem.java
+++ desapp-groupc1f-backend/src/main/java/model/OrderItem.java
@@ -3,33 +3,33 @@ package model;
 import utilities.Entity;
 
 public class OrderItem extends Entity {
+	private Menu menu;
+	private int cant;
+	  
 	public OrderItem() {  }
 	
 	public OrderItem(Menu m, int n) { 
 		this.menu = m;
 		this.cant = n;
 	}
-	
-  private Menu menu;
-  private int cant;
   
-  public int getPriceOrderItem() {
-	  return 0;
-  }
+	public int getPriceOrderItem() {
+	  	return 0;
+  	}
   
-  public Menu getMenu() {
-	return menu;
-  }
+  	public Menu getMenu() {
+	  return menu;
+  	}
 	
-  public void setMenu(Menu menu) {
-	this.menu = menu;
-  }
+  	public void setMenu(Menu menu) {
+  		this.menu = menu;
+  	}
 
-  public int getCant() {
-	  return cant;
-  }
+  	public int getCant() {
+  		return cant;
+  	}
 
-  public void setCant(int cant) {
-	  this.cant = cant;
-  }
+  	public void setCant(int cant) {
+  		this.cant = cant;
+  	}
 }
