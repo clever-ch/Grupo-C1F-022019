85455e9489907c04b8013fe4775b1b0a4fa50c8f
clever.ch <clever.chuky@gmail.com>
2019-09-21 21:05:03 -0300

    Create OrderItemFactory.java

---
+++ desapp-groupc1f-backend/src/test/java/modelFactories/OrderItemFactory.java
@@ -0,0 +1,18 @@
+package modelFactories;
+
+import model.Menu;
+import model.OrderItem;
+
+public class OrderItemFactory {
+	
+	public static OrderItem anyOrderItem() {
+		return new OrderItem();
+	}
+	
+	public static OrderItem createOrderItemWithOneMenuAndName(String menuName) {
+		Menu aMenu = MenuFactory.anyMenu();
+		aMenu.setMenuName(menuName);
+		
+		return new OrderItem(aMenu, 1);
+	}
+}
