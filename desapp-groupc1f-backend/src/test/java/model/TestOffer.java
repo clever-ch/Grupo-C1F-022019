e5c218d7f17c826b450443d7004e7ce05663307e
clever.ch <clever.chuky@gmail.com>
2019-09-24 15:08:45 -0300

    Create TestOffer.java

---
+++ desapp-groupc1f-backend/src/test/java/model/TestOffer.java
@@ -0,0 +1,43 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import constants.ForeignExchange;
+import model.factories.OfferFactory;
+
+public class TestOffer {
+
+	@Test
+	public void testOfferIsExcluded() {
+		Offer anOffer = OfferFactory.createCompleteOffer(8, 12, 80, ForeignExchange.ARS);
+		Offer offerExcluded = OfferFactory.createCompleteOffer(6, 7, 70, ForeignExchange.ARS);
+		
+		assertTrue(anOffer.offerIsExcluded(offerExcluded));
+	}
+
+	@Test
+	public void testMinorOfferIsIntersected() {
+		Offer anOffer = OfferFactory.createCompleteOffer(3, 6, 80, ForeignExchange.ARS);
+		Offer offerNotExcluded = OfferFactory.createCompleteOffer(2, 4, 70, ForeignExchange.ARS);
+		
+		assertFalse(anOffer.offerIsExcluded(offerNotExcluded));
+	}
+	
+	@Test
+	public void testMajorOfferIsIntersected() {
+		Offer anOffer = OfferFactory.createCompleteOffer(3, 6, 80, ForeignExchange.ARS);
+		Offer offerNotExcluded = OfferFactory.createCompleteOffer(4, 8, 70, ForeignExchange.ARS);
+		
+		assertFalse(anOffer.offerIsExcluded(offerNotExcluded));
+	}
+	
+	@Test
+	public void testOfferContainsAnotherOffer() {
+		Offer anOffer = OfferFactory.createCompleteOffer(3, 6, 80, ForeignExchange.ARS);
+		Offer offerNotExcluded = OfferFactory.createCompleteOffer(4, 5, 70, ForeignExchange.ARS);
+		
+		assertFalse(anOffer.offerIsExcluded(offerNotExcluded));
+	}
+}
