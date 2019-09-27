c46c206797b0062f5470b8630d3c3e205aeb6139
clever.ch <clever.chuky@gmail.com>
2019-09-27 17:01:57 -0300

    Update TestMenu.java

--- desapp-groupc1f-backend/src/test/java/model/TestMenu.java
+++ desapp-groupc1f-backend/src/test/java/model/TestMenu.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 
 import constants.Category;
 import constants.ForeignExchange;
-import exceptions.DuplicateOrderException;
 import exceptions.OfferRegistrationException;
 import model.factories.MenuFactory;
 import model.factories.OfferFactory;
