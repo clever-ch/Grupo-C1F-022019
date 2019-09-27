83543186e0022b8ff770bdda7e5304e7b4ee1292
clever.ch <clever.chuky@gmail.com>
2019-09-27 12:21:22 -0300

    Create ModelConstants.java

---
+++ desapp-groupc1f-backend/src/main/java/constants/ModelConstants.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class ModelConstants {
+	
+	private int minCategoriesMenu = 1;
+	private int minOffersMenu = 1;
+	
+	public int minCategoriesMenu() {
+		return minCategoriesMenu;
+	}
+	
+	public int minOffersMenu() {
+		return minOffersMenu;
+	}
+}
