aaf95e0ec2af3611084c3078d2648199b665530d
clever.ch <clever.chuky@gmail.com>
2019-09-28 15:55:41 -0300

    Create TestOrderItem.java

---
+++ desapp-groupc1f-backend/src/test/java/model/TestOrderItem.java
@@ -0,0 +1,40 @@
+package model;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import model.factories.MenuFactory;
+import model.factories.OrderItemFactory;
+
+public class TestOrderItem {
+
+	@Test
+	public void testOrderItemCanAddMenu() {
+		Menu anyMenu = MenuFactory.anyMenu();
+		
+		OrderItem orderItem = OrderItemFactory.anyOrderItem();
+		orderItem.setMenu(anyMenu);
+		
+		assertTrue(orderItem.getMenu().equals(anyMenu));
+	}
+
+	@Test
+	public void testOrderItemHasNoNumberOfMenus() {
+		OrderItem orderItem = OrderItemFactory.anyOrderItem();
+		
+		assertFalse(orderItem.getNumberMenus() > 0);
+	}
+	
+	@Test
+	public void testOrderItemNumberOfMenusIsNotvalid() {
+		OrderItem orderItem = OrderItemFactory.anyOrderItem();
+		
+		assertFalse(orderItem.isNumberOfMenusValid(0));
+	}
+	
+	@Test
+	public void testOrderItemNumberOfMenusIsvalid() {
+		OrderItem orderItem = OrderItemFactory.anyOrderItem();
+		
+		assertTrue(orderItem.isNumberOfMenusValid(1));
+	}
+}
