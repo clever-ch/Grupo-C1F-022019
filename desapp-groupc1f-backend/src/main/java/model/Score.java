d37ba9152d243ed933d4adb282efaf09ac88a08e
clever.ch <clever.chuky@gmail.com>
2019-09-27 21:55:20 -0300

    Update Score.java

--- desapp-groupc1f-backend/src/main/java/model/Score.java
+++ desapp-groupc1f-backend/src/main/java/model/Score.java
@@ -4,19 +4,47 @@ import constants.ScoreState;
 import utilities.Entity;
 
 public abstract class Score extends Entity {
-	private int score;
+	private int scoreValue;
 	private ScoreState scoreState;
 	private User user;
 	
-	public int getScore() {
-		return this.score;
+	public int getScoreValue() {
+		return this.scoreValue;
+	}
+	
+	public void setScoreValue(int value) {
+		this.scoreValue = value;
 	}
 	
 	public ScoreState getScoreState() {
 		return this.scoreState;
 	}
 	
+	public void setScoreState(ScoreState state) {
+		this.scoreState = state;
+	}
+	
 	public User getUser() {
 		return this.user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public boolean fullDataIsValid() {
+		return isValidScoreValue() & hasScoreState() & hasUserValid();   
+	}
+	
+	public boolean isValidScoreValue() {
+		return this.scoreValue > 0;
+	}
+	
+	public boolean hasScoreState() {
+		return this.scoreState != null;
+	}
+	
+	public boolean hasUserValid() {
+		return this.user != null;
 	}
 }
