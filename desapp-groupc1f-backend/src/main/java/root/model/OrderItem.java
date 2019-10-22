3aa29cf5c7703e10db255842b162b13b8ea4a770
clever.ch <clever.chuky@gmail.com>
2019-10-21 21:26:37 -0300

    Create OrderItem.java

---
+++ desapp-groupc1f-backend/src/main/java/root/model/OrderItem.java
@@ -0,0 +1,44 @@
+package root.model;
+
+import javax.persistence.OneToOne;
+import root.utilities.Entity;
+
+@javax.persistence.Entity
+public class OrderItem extends Entity{
+	
+	@OneToOne
+	private Menu menu;
+	private int numberMenus;
+	  
+	public OrderItem() {  }
+	
+	public OrderItem(Menu m, int n) { 
+		this.menu = m;
+		this.numberMenus = n;
+	}
+	
+	public int getPriceOrderItem() {
+		//TODO: Se puede calcular en base el precio del menu y la cantidad de menus
+	  	return 0;
+  	}
+  
+  	public Menu getMenu() {
+	  return menu;
+  	}
+	
+  	public void setMenu(Menu menu) {
+  		this.menu = menu;
+  	}
+
+  	public int getNumberMenus() {
+  		return numberMenus;
+  	}
+
+  	public void setNumberMenus(int cant) {
+  		this.numberMenus = cant;
+  	}
+  	
+  	public boolean isNumberOfMenusValid(int number) {
+  		return number > 0;
+  	}
+}
