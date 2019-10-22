35ee951f12f5a7b11e3260e18d6f3c5b7c12dbbf
clever.ch <clever.chuky@gmail.com>
2019-10-21 21:25:57 -0300

    Create DuplicateMenuException.java

---
+++ desapp-groupc1f-backend/src/main/java/root/exceptions/DuplicateMenuException.java
@@ -0,0 +1,9 @@
+package root.exceptions;
+
+@SuppressWarnings("serial")
+public class DuplicateMenuException extends Exception {
+	public DuplicateMenuException(String msj) {
+		super(msj);
+	}
+
+}
