2a8b2222149a2443efb84b8f823deec645b3f1d3
clever.ch <clever.chuky@gmail.com>
2019-10-21 21:26:02 -0300

    Create DuplicateOrderHistoryException.java

---
+++ desapp-groupc1f-backend/src/main/java/root/exceptions/DuplicateOrderHistoryException.java
@@ -0,0 +1,8 @@
+package root.exceptions;
+
+@SuppressWarnings("serial")
+public class DuplicateOrderHistoryException extends Exception {
+	public DuplicateOrderHistoryException(String msj) {
+		super(msj);
+	}
+}
