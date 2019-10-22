ba75f116f077d66237b54fd83d90d2fe90bcd17a
clever.ch <clever.chuky@gmail.com>
2019-10-21 21:25:48 -0300

    Create MenuController.java

---
+++ desapp-groupc1f-backend/src/main/java/root/controller/MenuController.java
@@ -0,0 +1,39 @@
+package root.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import root.model.Menu;
+import root.service.MenuService;
+
+@Controller
+public class MenuController {
+
+	@Autowired
+	private MenuService menuService;
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "home.jsp";
+	}
+	
+	@RequestMapping(value = "/getMenu", method= RequestMethod.GET)
+	public ModelAndView getMenu(@RequestParam long id)
+	{
+		ModelAndView mv=new ModelAndView("showMenu.jsp");
+		Menu aMenu=menuService.getMenuRepository().findById(id).orElse(new Menu());	
+		
+		mv.addObject(aMenu);
+		return mv;
+	}
+	
+	//@RequestMapping("/menus")
+    //@ResponseBody
+    //public ResponseEntity<List> getMenus() {
+    //    return new ResponseEntity(menuService.getMenus(), HttpStatus.OK);
+    //}
+}
