c9fb75de7459433616ab77152e74af7c2e847921
clever.ch <clever.chuky@gmail.com>
2019-10-21 21:26:43 -0300

    Create Score.java

---
+++ desapp-groupc1f-backend/src/main/java/root/model/Score.java
@@ -0,0 +1,57 @@
+package root.model;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToOne;
+import root.constants.ScoreState;
+import root.utilities.Entity;
+
+public abstract class Score extends Entity {
+	private int scoreValue;
+	
+	@Enumerated(EnumType.STRING)
+	private ScoreState scoreState;
+	
+	@OneToOne
+	private User user;
+	
+	public int getScoreValue() {
+		return this.scoreValue;
+	}
+	
+	public void setScoreValue(int value) {
+		this.scoreValue = value;
+	}
+	
+	public ScoreState getScoreState() {
+		return this.scoreState;
+	}
+	
+	public void setScoreState(ScoreState state) {
+		this.scoreState = state;
+	}
+	
+	public User getUser() {
+		return this.user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public boolean fullDataIsValid() {
+		return isValidScoreValue() & hasScoreState() & hasUserValid();   
+	}
+	
+	public boolean isValidScoreValue() {
+		return this.scoreValue > 0;
+	}
+	
+	public boolean hasScoreState() {
+		return this.scoreState != null;
+	}
+	
+	public boolean hasUserValid() {
+		return this.user != null;
+	}
+}
