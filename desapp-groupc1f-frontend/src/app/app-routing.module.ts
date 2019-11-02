cb58405599437e4a9e957e778698f681c0274192
clever-ch <clever.chuky@gmail.com>
2019-11-02 18:25:06 -0300

    Update app-routing.module.ts

--- desapp-groupc1f-frontend/src/app/app-routing.module.ts
+++ desapp-groupc1f-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountListComponent } from './account-list/account-list.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'account', pathMatch: 'full' },
+  { path: 'accounts', component: AccountListComponent },
+  { path: 'add', component: CreateAccountComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
