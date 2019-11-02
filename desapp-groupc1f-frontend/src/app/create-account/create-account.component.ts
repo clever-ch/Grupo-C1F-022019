216016bdb0fbc57a86593cfdd9a176309005ee9d
clever-ch <clever.chuky@gmail.com>
2019-11-02 18:26:09 -0300

    Create create-account.component.ts

---
+++ desapp-groupc1f-frontend/src/app/create-account/create-account.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
+import { Account } from '../account';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-account',
+  templateUrl: './create-account.component.html',
+  styleUrls: ['./create-account.component.css']
+})
+export class CreateAccountComponent implements OnInit {
+
+  account: Account = new Account();
+  submitted = false;
+
+  constructor(private accountService: AccountService,
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  newAccount(): void {
+    this.submitted = false;
+    this.account = new Account();
+  }
+
+  save() {
+    this.accountService.createAccount(this.account)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.account = new Account();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();    
+  }
+
+  gotoList() {
+    this.router.navigate(['/accounts']);
+  }
+
+}
