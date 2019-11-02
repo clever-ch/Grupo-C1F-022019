f9218ba222cab589d1f755c59454f48ed59817a7
clever-ch <clever.chuky@gmail.com>
2019-11-02 18:25:02 -0300

    Create account.ts

---
+++ desapp-groupc1f-frontend/src/app/account.ts
@@ -0,0 +1,10 @@
+export class Account {
+    id: number;
+    userName: string;
+    name: string;
+    surname: string;
+    eMail: string;
+    phone: string;
+    location: string;
+    direction: string;
+}
