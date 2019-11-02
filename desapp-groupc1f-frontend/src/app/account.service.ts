c00bc8a93205d60aa3814bb6de1597ad5394616f
clever-ch <clever.chuky@gmail.com>
2019-11-02 18:24:52 -0300

    Create account.service.ts

---
+++ desapp-groupc1f-frontend/src/app/account.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  private baseUrl = 'http://localhost:8080/api/v1/accounts';
+
+  constructor(private http: HttpClient) { }
+
+  getAccount(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  getAccountList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  createAccount(account: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, account);
+  }
+}
