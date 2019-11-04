dbbb72b83c1596f649c56f01058caac65c5f942c
clever.ch <clever.chuky@gmail.com>
2019-11-04 19:27:24 -0300

    Update MenuController.java

--- desapp-groupc1f-backend/src/main/java/root/controller/MenuController.java
+++ desapp-groupc1f-backend/src/main/java/root/controller/MenuController.java
@@ -1,39 +1,76 @@
 package root.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
+import root.controller.exception.ResourceNotFoundException;
 import root.model.Menu;
-import root.service.MenuService;
+import root.repository.MenuRepository;
 
-@Controller
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1")
 public class MenuController {
 
 	@Autowired
-	private MenuService menuService;
+	private MenuRepository menuRepository;
 	
-	@RequestMapping("/")
-	public String home()
-	{
-		return "home.jsp";
+	@GetMapping("/menus")
+	public List<Menu> getAllMenus() {
+		return menuRepository.findAll();
 	}
 	
-	@RequestMapping(value = "/getMenu", method= RequestMethod.GET)
-	public ModelAndView getMenu(@RequestParam long id)
-	{
-		ModelAndView mv=new ModelAndView("showMenu.jsp");
-		Menu aMenu=menuService.getMenuRepository().findById(id).orElse(new Menu());	
-		
-		mv.addObject(aMenu);
-		return mv;
+	@PostMapping("/menus")
+	public Menu createMenu(@Valid @RequestBody Menu menu) {
+		return menuRepository.save(menu);
 	}
 	
-	//@RequestMapping("/menus")
-    //@ResponseBody
-    //public ResponseEntity<List> getMenus() {
-    //    return new ResponseEntity(menuService.getMenus(), HttpStatus.OK);
-    //}
+	@GetMapping("/menus/{id}")
+	public ResponseEntity<Menu> getMenuById(@PathVariable(value = "id") Long menuId)
+			throws ResourceNotFoundException {
+		Menu menu = menuRepository.findById(menuId)
+				.orElseThrow(() -> new ResourceNotFoundException("Menu not found for this id :: " + menuId));
+		return ResponseEntity.ok().body(menu);
+	}
+	
+	@PutMapping("/menus/{id}")
+	public ResponseEntity<Menu> updateMenu(@PathVariable(value = "id") Long menuId,
+			@Valid @RequestBody Menu menuDetails) throws ResourceNotFoundException {
+		Menu menu = menuRepository.findById(menuId)
+				.orElseThrow(() -> new ResourceNotFoundException("Menu not found for this id :: " + menuId));
+
+		menu.setMenuName(menuDetails.getMenuName());
+		menu.setDescription(menuDetails.getDescription());
+		menu.setPrice(menuDetails.getPrice());
+		menu.setValidFromDate(menuDetails.getValidFromDate());
+		menu.setValidToDate(menuDetails.getValidToDate());
+		menu.setDeliveryPrice(menuDetails.getDeliveryPrice());
+		menu.setAvgDeliveryTime(menuDetails.getAvgDeliveryTime());
+		final Menu updatedMenu = menuRepository.save(menu);
+		return ResponseEntity.ok(updatedMenu);
+	}
+	
+	@DeleteMapping("/menus/{id}")
+	public Map<String, Boolean> deleteMenu(@PathVariable(value = "id") Long menuId)
+			throws ResourceNotFoundException {
+		Menu menu = menuRepository.findById(menuId)
+				.orElseThrow(() -> new ResourceNotFoundException("Menu not found for this id :: " + menuId));
+
+		menuRepository.delete(menu);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
+	}
 }
