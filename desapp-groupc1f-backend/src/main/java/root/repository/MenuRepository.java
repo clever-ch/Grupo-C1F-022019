5eff6dc69e25921ff8763a3706366b2733f491b9
clever.ch <clever.chuky@gmail.com>
2019-11-04 19:27:36 -0300

    Update MenuRepository.java

--- desapp-groupc1f-backend/src/main/java/root/repository/MenuRepository.java
+++ desapp-groupc1f-backend/src/main/java/root/repository/MenuRepository.java
@@ -1,10 +1,10 @@
 package root.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import root.model.Menu;
 
 @Repository
-public interface MenuRepository extends CrudRepository<Menu, Long> {
+public interface MenuRepository extends JpaRepository<Menu, Long>{
 
 }
