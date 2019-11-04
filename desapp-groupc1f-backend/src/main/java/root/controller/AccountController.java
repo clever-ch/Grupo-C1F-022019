394959c7eb8c909301944d52d3b8e57ee83abe47
clever.ch <clever.chuky@gmail.com>
2019-11-04 19:27:03 -0300

    Update AccountController.java

--- desapp-groupc1f-backend/src/main/java/root/controller/AccountController.java
+++ desapp-groupc1f-backend/src/main/java/root/controller/AccountController.java
@@ -1,14 +1,21 @@
 package root.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import root.controller.exception.ResourceNotFoundException;
 import root.model.Account;
 import root.repository.AccountRepository;
 
@@ -28,5 +35,41 @@ public class AccountController {
 	@PostMapping("/accounts")
 	public Account createAccount(@Valid @RequestBody Account account) {
 		return accountRepository.save(account);
+	}
+	
+	@GetMapping("/accounts/{id}")
+	public ResponseEntity<Account> getAccountById(@PathVariable(value = "id") Long accountId)
+			throws ResourceNotFoundException {
+		Account account = accountRepository.findById(accountId)
+				.orElseThrow(() -> new ResourceNotFoundException("Account not found for this id :: " + accountId));
+		return ResponseEntity.ok().body(account);
+	}
+	
+	@PutMapping("/accounts/{id}")
+	public ResponseEntity<Account> updateAccount(@PathVariable(value = "id") Long accountId,
+			@Valid @RequestBody Account accountDetails) throws ResourceNotFoundException {
+		Account account = accountRepository.findById(accountId)
+				.orElseThrow(() -> new ResourceNotFoundException("Account not found for this id :: " + accountId));
+
+		account.setSurname(accountDetails.getSurname());
+		account.setName(accountDetails.getName());
+		account.setEmail(accountDetails.getEmail());
+		account.setPhone(accountDetails.getPhone());
+		account.setLocation(accountDetails.getLocation());
+		account.setDirection(accountDetails.getDirection());
+		final Account updatedAccount = accountRepository.save(account);
+		return ResponseEntity.ok(updatedAccount);
+	}
+	
+	@DeleteMapping("/accounts/{id}")
+	public Map<String, Boolean> deleteAccount(@PathVariable(value = "id") Long accountId)
+			throws ResourceNotFoundException {
+		Account account = accountRepository.findById(accountId)
+				.orElseThrow(() -> new ResourceNotFoundException("Account not found for this id :: " + accountId));
+
+		accountRepository.delete(account);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
 	}
 }
