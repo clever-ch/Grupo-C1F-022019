cfa4825bb6a5b40c590874e03fbedc430f4b6f83
clever.ch <clever.chuky@gmail.com>
2019-12-02 20:33:57 -0300

    Update create-menu.component.ts

--- desapp-groupc1f-frontend/src/app/components/create-menu/create-menu.component.ts
+++ desapp-groupc1f-frontend/src/app/components/create-menu/create-menu.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { DeliveryInfo } from 'src/app/model/delivery-info'
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { LinkedList } from 'ngx-bootstrap';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-create-menu',
@@ -18,12 +19,35 @@ export class CreateMenuComponent implements OnInit {
   submitted = false;
   dropdownSettings: IDropdownSettings = {};
   ddlCategories = [];
-  selectedItems = new LinkedList<any>();
+  selectedCategories = new LinkedList<any>();
+  ddlShifts = [];
+  selectedShifts = new LinkedList<any>();
   maxChars = 300;
+  hasDelivery = false;
+  ddlhsAttention = [];
+  selectedhsAttention = new LinkedList<Date>();
+
+  pipe = new DatePipe('en-US');
+  time_1 = new Date(2019,1,1,8,0);
+  time_2 = new Date(2019,1,1,12,0);
+  time_3 = new Date(2019,1,1,17,0);
+  time_4 = new Date(2019,1,1,21,0);
+  
+  hour_1 = this.pipe.transform(this.time_1, 'shortTime');
+  hour_2 = this.pipe.transform(this.time_2, 'shortTime');
+  hour_3 = this.pipe.transform(this.time_3, 'shortTime');
+  hour_4 = this.pipe.transform(this.time_4, 'shortTime');
 
   constructor(private menuService: MenuService, private router: Router) { }
 
   ngOnInit() {
+    this.dropdownSettings = {
+      singleSelection: false,
+      defaultOpen: false,
+      idField: 'item_id',
+      textField: 'item_text'
+    };
+
     this.ddlCategories = [
       { item_id: 1, item_text: 'Pizza' },
       { item_id: 2, item_text: 'Beer' },
@@ -34,22 +58,55 @@ export class CreateMenuComponent implements OnInit {
       { item_id: 7, item_text: 'Vegan' }
     ];
 
-    this.dropdownSettings = {
-      singleSelection: false,
-      defaultOpen: false,
-      idField: 'item_id',
-      textField: 'item_text'
-    };
+    this.ddlShifts = [
+      { item_id: 1, item_text: 'Morning' },
+      { item_id: 2, item_text: 'Afternoon' },
+      { item_id: 3, item_text: 'Night' },
+    ];
+
+    this.ddlhsAttention = [
+      { item_id: 1, item_text: this.hour_1 },
+      { item_id: 2, item_text: this.hour_2 },
+      { item_id: 3, item_text: this.hour_3 },
+      { item_id: 4, item_text: this.hour_4 },
+    ];
   }
 
   onItemSelect(item: any) {
-    console.log('onItem Select', item);
-    this.selectedItems.add(item);
+    this.selectedCategories.add(item);
   }
 
   onItemDeSelect(item: any) {
-    console.log('onItem DeSelect', item);
-    this.selectedItems.remove(item);
+    this.selectedCategories.remove(item);
+  }
+
+  onShiftSelect(item: any) {
+    this.selectedShifts.add(item);
+  }
+
+  onShiftDeSelect(item: any) {
+    this.selectedShifts.remove(item);
+  }
+
+  onHourSelect(item: any) {
+    this.selectedhsAttention.add(item);
+  }
+
+  onHourDeSelect(item: any) {
+    this.selectedhsAttention.remove(item);
+  }
+  
+  createDeliveryInfo(): DeliveryInfo {
+    var deliveryInfo = new DeliveryInfo();
+    deliveryInfo.shiftsAvailable = this.selectedShifts.toArray();
+    deliveryInfo.hoursAttention = this.selectedhsAttention.toArray();
+    deliveryInfo.hasDelivery = this.hasDelivery;
+
+    return deliveryInfo;
+  }
+
+  chkHasDeliveryEvent(e){
+    this.hasDelivery= e.target.checked;
   }
 
   newMenu(): void {
@@ -58,8 +115,8 @@ export class CreateMenuComponent implements OnInit {
   }
 
   save() {
-    this.menu.categories = this.selectedItems.toArray();
-    this.menu.deliveryInfo = this.deliveryInfo;
+    this.menu.categories = this.selectedCategories.toArray();
+    this.menu.deliveryInfo = this.createDeliveryInfo();
     this.menuService.createMenu(this.menu)
       .subscribe(data => console.log(data), error => console.log(error));
     this.menu = new Menu();
