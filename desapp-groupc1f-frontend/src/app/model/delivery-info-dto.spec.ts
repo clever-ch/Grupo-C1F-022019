08939fc99e4f78fb8369d88bdfdb2d178a2229f4
clever-ch <clever.chuky@gmail.com>
2019-12-07 18:02:31 -0300

    Create delivery-info-dto.spec.ts

---
+++ desapp-groupc1f-frontend/src/app/model/delivery-info-dto.spec.ts
@@ -0,0 +1,7 @@
+import { DeliveryInfoDTO } from './delivery-info-dto';
+
+describe('DeliveryInfoDTO', () => {
+  it('should create an instance', () => {
+    expect(new DeliveryInfoDTO()).toBeTruthy();
+  });
+});
