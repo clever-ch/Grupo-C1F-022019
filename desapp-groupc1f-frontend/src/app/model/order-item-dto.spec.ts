3d912af4e63eb5184f75561cffb87f8db57b70fc
clever.ch <clever.chuky@gmail.com>
2019-12-17 18:12:18 -0300

    Create order-item-dto.spec.ts

---
+++ desapp-groupc1f-frontend/src/app/model/order-item-dto.spec.ts
@@ -0,0 +1,7 @@
+import { OrderItemDTO } from './order-item-dto';
+
+describe('OrderItemDTO', () => {
+  it('should create an instance', () => {
+    expect(new OrderItemDTO()).toBeTruthy();
+  });
+});
