441c2fc36b3230050568ae8d78fd4e34cf14a805
clever.ch <clever.chuky@gmail.com>
2019-12-17 18:11:16 -0300

    Update app.component.ts

--- desapp-groupc1f-frontend/src/app/app.component.ts
+++ desapp-groupc1f-frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import { AuthService } from "../../src/app/services/auth-service/auth.service";
+import { CustomerService } from "../../src/app/services/customer-service/customer.service";
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,13 @@ import { AuthService } from "../../src/app/services/auth-service/auth.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {  
-  constructor(private translate: TranslateService, public authService: AuthService) {
+  constructor(private translate: TranslateService, public authService: AuthService, public customerService: CustomerService) {
     translate.setDefaultLang('en');
   }
 
 useLanguage(language: string) {
     this.translate.use(language);
+    console.log("Resultado CustomerService");
+    console.log(this.customerService.getCustomerList());
   }
 }
