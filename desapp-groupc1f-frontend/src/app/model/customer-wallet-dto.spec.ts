bf0691f038b34199d0457f3b06f9dffa4a32fd3a
clever.ch <clever.chuky@gmail.com>
2019-12-17 18:12:03 -0300

    Create customer-wallet-dto.spec.ts

---
+++ desapp-groupc1f-frontend/src/app/model/customer-wallet-dto.spec.ts
@@ -0,0 +1,7 @@
+import { CustomerWalletDTO } from './customer-wallet-dto';
+
+describe('CustomerWalletDTO', () => {
+  it('should create an instance', () => {
+    expect(new CustomerWalletDTO()).toBeTruthy();
+  });
+});
