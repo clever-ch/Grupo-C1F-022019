3e8f31463e25899a3a187ed817acc65e1a2a9b27
clever.ch <clever.chuky@gmail.com>
2019-12-17 18:10:34 -0300

    Update CustomerWallet.java

--- desapp-groupc1f-backend/src/main/java/root/model/CustomerWallet.java
+++ desapp-groupc1f-backend/src/main/java/root/model/CustomerWallet.java
@@ -1,19 +1,38 @@
 package root.model;
 
-import root.exceptions.OptionNotAvailableException;
+import javax.persistence.OneToOne;
+
+import root.exceptions.CantTakeCreditException;
 
 @javax.persistence.Entity
 public class CustomerWallet extends Wallet {
 	
-    @Override
+	@OneToOne
+	private Customer customer;
+	
+    public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	@Override
     public void loadCredit(double n) {
     	Double actualAmount = this.getAmount();
         this.setAmount(actualAmount + n);
     }
 
     @Override
-    public void takeCredit(double n) throws OptionNotAvailableException{
-        throw new OptionNotAvailableException("Opcion no disponible para este perfil");
+    public void takeCredit(double n) throws CantTakeCreditException {
+    	Double actualAmount = this.getAmount();
+        
+    	if ((actualAmount - n) >= 0){
+                this.setAmount(actualAmount - n);
+            } else {
+                throw new CantTakeCreditException("El monto que desea quitar excede lo disponible en su billetera");
+            }
     }
 
     @Override
