8aa8edcb1760726620b606d12ad091bac108c983
clever-ch <clever.chuky@gmail.com>
2019-12-17 21:26:43 -0300

    Update data.sql

--- desapp-groupc1f-backend/src/main/resources/data.sql
+++ desapp-groupc1f-backend/src/main/resources/data.sql
@@ -5,7 +5,7 @@ insert into Account values (2, 'calle falsa 123', 'jose@gmail.com', 'Quilmes', '
 
 
 --SECCIÓN BILLETERAS
-insert into CUSTOMER_WALLET values (1, 1000.0, null);
+insert into CUSTOMER_WALLET values (1, 10000.0, null);
 insert into PROVIDER_WALLET values (1, 100.0, null);
 
 
