08e293e0f3424ac558cb5b6f8ffaee0b5deb02d7
clever-ch <clever.chuky@gmail.com>
2019-12-17 21:26:47 -0300

    Update app.component.ts

--- desapp-groupc1f-frontend/src/app/app.component.ts
+++ desapp-groupc1f-frontend/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { Observable } from "rxjs";
 import {TranslateService} from '@ngx-translate/core';
 import { AuthService } from "../../src/app/services/auth-service/auth.service";
 import { CustomerService } from "../../src/app/services/customer-service/customer.service";
+import { CustomerDTO } from 'src/app/model/customerDTO';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {  
+export class AppComponent { 
+
+  customers: Observable<CustomerDTO[]>;
+
   constructor(private translate: TranslateService, public authService: AuthService, public customerService: CustomerService) {
     translate.setDefaultLang('en');
   }
@@ -16,6 +21,7 @@ export class AppComponent {
 useLanguage(language: string) {
     this.translate.use(language);
     console.log("Resultado CustomerService");
-    console.log(this.customerService.getCustomerList());
+    this.customerService.getCustomerList().subscribe(data => this.customers = data);
+    console.log(this.customers);
   }
 }
